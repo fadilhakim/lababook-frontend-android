326f960a2f9a73938863c9c99c2d6550be160628
akbarsahatas <iam@akbarsahata.id>
2019-12-05 09:29:41 +0700

    init add contact

--- pages/Kontak.js
+++ pages/Kontak.js
@@ -9,6 +9,29 @@ import {
   MaterialIcons,
   AntDesign
 } from '@expo/vector-icons'
+import * as Permissions from 'expo-permissions'
+import * as Contacts from 'expo-contacts'
+
+async function showContact () {
+  try {
+    const { status } = await Permissions.askAsync(Permissions.CONTACTS)
+
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [
+          Contacts.Fields.Name,
+          Contacts.Fields.PhoneNumbers
+        ]
+      })
+
+      if (data.length > 0) {
+        console.log(data)
+      }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 function Kontak () {
   return (
@@ -47,6 +70,178 @@ function Kontak () {
           />
         </TouchableNativeFeedback>
       </View>
+      <View>
+        <View
+          style={{
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            width: '100%'
+          }}
+        >
+          <View
+            style={{
+              flexDirection: 'row'
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: '#aaa',
+                paddingLeft: 14,
+                paddingRight: 14,
+                paddingBottom: 3,
+                margin: 5,
+                marginLeft: 15,
+                marginRight: 10,
+                borderRadius: 100
+              }}
+            >
+              <Text
+                style={{
+                  color: 'white',
+                  fontSize: 36
+                }}
+              >
+                A
+              </Text>
+            </View>
+
+            <View
+              style={{
+                flexDirection: 'column',
+                justifyContent: 'center'
+              }}
+            >
+              <View>
+                <Text style={{ fontSize: 18 }}>Aan Siguna</Text>
+              </View>
+              <View style={{ flexDirection: 'row' }}>
+                <AntDesign
+                  name='clockcircleo'
+                  size={12}
+                  style={{ marginTop: 3 }}
+                />
+                <Text> 3 Hari lalu</Text>
+              </View>
+            </View>
+          </View>
+
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'flex-end',
+              paddingRight: 20
+            }}
+          >
+            <Text style={{ color: 'red' }}>
+              Anda berikan
+            </Text>
+            <Text style={{ fontSize: 18, color: 'red' }}>
+              Rp. 2.000.000
+            </Text>
+          </View>
+        </View>
+        <View
+          style={{
+            justifyContent: 'space-between',
+            flexDirection: 'row',
+            width: '100%'
+          }}
+        >
+          <View
+            style={{
+              flexDirection: 'row'
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: '#aaa',
+                paddingLeft: 14,
+                paddingRight: 14,
+                paddingBottom: 3,
+                margin: 5,
+                marginLeft: 15,
+                marginRight: 10,
+                borderRadius: 100
+              }}
+            >
+              <Text
+                style={{
+                  color: 'white',
+                  fontSize: 36
+                }}
+              >
+                S
+              </Text>
+            </View>
+
+            <View
+              style={{
+                flexDirection: 'column',
+                justifyContent: 'center'
+              }}
+            >
+              <View>
+                <Text style={{ fontSize: 18 }}>Sibutar butar</Text>
+              </View>
+              <View style={{ flexDirection: 'row' }}>
+                <AntDesign
+                  name='clockcircleo'
+                  size={12}
+                  style={{ marginTop: 3 }}
+                />
+                <Text> 4 Hari lalu</Text>
+              </View>
+            </View>
+          </View>
+
+          <View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'flex-end',
+              paddingRight: 20
+            }}
+          >
+            <Text style={{ color: 'green' }}>
+              Anda berikan
+            </Text>
+            <Text style={{ fontSize: 18, color: 'green' }}>
+              Rp. 2.000.000
+            </Text>
+          </View>
+        </View>
+      </View>
+
+      <TouchableNativeFeedback
+        onPress={() => showContact()}
+      >
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'absolute',
+            zIndex: 999,
+            right: 25,
+            bottom: 25,
+            borderWidth: 3,
+            borderColor: '#444',
+            borderRadius: 50,
+            padding: 8
+          }}
+        >
+          <AntDesign
+            name='plus'
+            size={22}
+          />
+          <Text
+            style={{
+              fontSize: 20
+            }}
+          >
+            Tambah Kontak
+          </Text>
+        </View>
+      </TouchableNativeFeedback>
     </View>
   )
 }
