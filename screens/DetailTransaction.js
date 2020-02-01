50e55212a46a71095e9f0b211e7a0a95a0aee031
kunglaw <alhusna901@gmail.com>
2020-02-01 21:23:52 +0700

    update

--- screens/DetailTransaction.js
+++ screens/DetailTransaction.js
@@ -23,14 +23,14 @@ class DetailTransaction extends Component {
     super()
 
     this.state = {
-      contactTransactions:[],
-      userId:"",
-      totalDebit:0,
-      totalCredit:0,
-      totalTransaction:0,
+      contactTransactions: [],
+      userId: "",
+      totalDebit: 0,
+      totalCredit: 0,
+      totalTransaction: 0,
       tableHead: [],
       tableData: [
-     
+
       ],
     }
   }
@@ -43,66 +43,68 @@ class DetailTransaction extends Component {
     const params = this.props.navigation.state.params
 
     this.setState({
-      userId:params.userId
+      userId: params.userId
     })
 
     transactionApi.getTransactionByContact(params.contactId)
-    .then(res => {
-      
-      
-      const dtHeader= ["Total"]
-      const dtTable = []
+      .then(res => {
 
-      res.data.map((item) => {
-        var row = []
 
-        if( item.type === "debit") {
-          row.push(item.description,numberFormat(item.amount),"")
-          
-          this.setState({
-            totalDebit: this.state.totalDebit + item.amount,
-            totalTransaction: this.state.totalTransaction + item.amount
-          })
-        } else{
-          row.push(item.description,"",numberFormat(item.amount))
-          this.setState({
-            totalCredit: this.state.totalCredit + item.amount,
-            totalTransaction: this.state.totalTransaction + item.amount
-          })
-        }
-        
-        
-        dtTable.push(row)
+        const dtHeader = ["Total"]
+        const dtTable = []
+
+        res.data.map((item) => {
+          var row = []
+
+          if (item.type === "debit") {
+            row.push(item.description, "", numberFormat(item.amount))
+
+            this.setState({
+              totalDebit: this.state.totalDebit + item.amount,
+              totalTransaction: this.state.totalTransaction + item.amount
+            })
+          } else {
+            row.push(item.description, numberFormat(item.amount), "")
+            this.setState({
+              totalCredit: this.state.totalCredit + item.amount,
+              totalTransaction: this.state.totalTransaction - item.amount
+
+            })
+          }
+
+
+          dtTable.push(row)
+        })
+
+        dtHeader.push(`Anda Berikan \n ${numberFormat(this.state.totalCredit)}`, `Anda Dapatkan \n ${numberFormat(this.state.totalDebit)}`)
+        this.setState({
+          contactTransactions: this.state.contactTransactions.concat(res.data),
+          tableHead: this.state.tableHead.concat(dtHeader),
+          tableData: this.state.tableData.concat(dtTable),
+
+        })
       })
-
-      dtHeader.push(`Anda Berikan \n ${ numberFormat(this.state.totalDebit)}`, `Anda Dapatkan \n ${numberFormat(this.state.totalCredit)}`)
-      this.setState({
-        contactTransactions:this.state.contactTransactions.concat(res.data),
-        tableHead:this.state.tableHead.concat(dtHeader),
-        tableData:this.state.tableData.concat(dtTable)
+      .catch(err => {
+        alert(err)
       })
-    })
-    .catch(err => {
-      alert(err)
-    })
 
   }
 
   signOut() {
     AsyncStorage.removeItem('userToken')
-    .then(() => navigation.navigate('AuthLoading'))
+      .then(() => navigation.navigate('AuthLoading'))
   }
 
   render() {
     const { navigation } = this.props
     const params = this.props.navigation.state.params
-    
-    var dataTransaction =   <View style={BaseStyle.blmWrap}>
+
+    var dataTransaction = <View style={BaseStyle.blmWrap}>
       <Text style={BaseStyle.blmAdaText}>Belum Anda Transaksi</Text>
     </View>
 
-    if( this.state.contactTransactions.length > 0) {
-      dataTransaction =   <ScrollView ><Table>
+    if (this.state.contactTransactions.length > 0) {
+      dataTransaction = <ScrollView ><Table>
         <Row data={this.state.tableHead} style={styles.head} textStyle={styles.text} />
         {
           this.state.tableData.map((rowData, index) => {
@@ -110,7 +112,7 @@ class DetailTransaction extends Component {
               key={index}
               data={rowData}
               textStyle={{ textAlign: "left", borderRightColor: "#f3f3f3", borderRightWidth: 3, paddingLeft: 5 }}
-              style={[{ backgroundColor: 'white', minHeight: 20, alignItems: 'center', paddingLeft: 5, paddingTop : 8, paddingBottom : 8 }, index % 2 && { backgroundColor: "#F7F6E7", minHeight: 20, alignItems: 'center', paddingLeft: 5, paddingTop : 8, paddingBottom : 8 }]}
+              style={[{ backgroundColor: 'white', minHeight: 20, alignItems: 'center', paddingLeft: 5, paddingTop: 8, paddingBottom: 8 }, index % 2 && { backgroundColor: "#F7F6E7", minHeight: 20, alignItems: 'center', paddingLeft: 5, paddingTop: 8, paddingBottom: 8 }]}
             ></Row>
           })
         }
@@ -118,7 +120,7 @@ class DetailTransaction extends Component {
       </Table></ScrollView>
 
     }
-  
+
     return (
       <View style={BaseStyle.container}>
         <View style={BaseStyle.headerBlue}>
@@ -145,36 +147,40 @@ class DetailTransaction extends Component {
           </View>
         </View>
         <View style={BaseStyle.headerBtm}>
-          <Text>Total : { numberFormat(this.state.totalTransaction) } </Text>
+          <Text>Total : {numberFormat(this.state.totalTransaction)} </Text>
           <Text>Pengingat : - </Text>
         </View>
-        
-        { dataTransaction }
-        
+
+        {dataTransaction}
+
         <View style={BaseStyle.btnWrap}>
-  
-          <TouchableNativeFeedback onPress={() => { NavigationService.navigate("AddTransactions",{
-          
-            transactionType:"credit",
-            name: params.name,
-            phoneNumber: params.phoneNumber,
-            contactInitial: params.contactInitial,
-            contactId:params.contactId,
-            userId:this.state.userId,
-            totalTransaction:params.totalTransaction
-          }) }}>
+
+          <TouchableNativeFeedback onPress={() => {
+            NavigationService.navigate("AddTransactions", {
+
+              transactionType: "credit",
+              name: params.name,
+              phoneNumber: params.phoneNumber,
+              contactInitial: params.contactInitial,
+              contactId: params.contactId,
+              userId: this.state.userId,
+              totalTransaction: params.totalTransaction
+            })
+          }}>
             <View style={BaseStyle.btnBerikan}><Text style={BaseStyle.btnText}>ANDA BERIKAN</Text></View>
           </TouchableNativeFeedback>
-          <TouchableNativeFeedback onPress={() => { NavigationService.navigate("AddTransactions",{
-            transactionType:"debit",
-            name: params.name,
-            phoneNumber: params.phoneNumber,
-            contactInitial: params.contactInitial,
-            contactId:params.contactId,
-            userId:params.userId,
-            totalTransaction:params.totalTransaction,
-            userId:this.state.userId
-          }) }}>
+          <TouchableNativeFeedback onPress={() => {
+            NavigationService.navigate("AddTransactions", {
+              transactionType: "debit",
+              name: params.name,
+              phoneNumber: params.phoneNumber,
+              contactInitial: params.contactInitial,
+              contactId: params.contactId,
+              userId: params.userId,
+              totalTransaction: params.totalTransaction,
+              userId: this.state.userId
+            })
+          }}>
             <View onPress={() => { NavigationService.navigate("Home") }} style={BaseStyle.btnDapatkan}>
               <Text style={BaseStyle.btnText}>ANDA DAPATKAN</Text></View>
           </TouchableNativeFeedback>
@@ -183,7 +189,7 @@ class DetailTransaction extends Component {
     )
   }
 
-  
+
 }
 
 export default DetailTransaction
@@ -191,7 +197,7 @@ export default DetailTransaction
 const styles = StyleSheet.create({
 
   head: { height: 50, backgroundColor: '#f1f8ff', paddingLeft: 5 },
-  text: { paddingLeft: 5, textAlign: "left"},
+  text: { paddingLeft: 5, textAlign: "left" },
   Rows: {
 
     borderBottomColor: "white",
