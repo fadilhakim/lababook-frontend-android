14b0e10c216b6777241c71a1cde96004e1c738aa
kunglaw <alhusna901@gmail.com>
2020-02-27 21:38:04 +0700

    update

--- api/contact.js
+++ api/contact.js
@@ -3,35 +3,35 @@ import axios from "axios"
 import { api } from '../config/url'
 import { connect } from 'react-redux'
 
-export default  class ContactAPI {
+export default class ContactAPI {
     getContacts(params) {
-        console.log(api+"/contacts")
-        return axios.get(api+"/contacts",{
-            params:{
-                bookId:params.bookId,
-                sort:params.sort,
-                filter:params.filter
+        console.log(api + "/contacts")
+        return axios.get(api + "/contacts", {
+            params: {
+                bookId: params.bookId,
+                sort: params.sort,
+                filter: params.filter
             },
             headers: {
-                'Content-Type': 'application/json',
-                Authorization : 'Bearer ' + params.token
+                'Accept': 'application/json',
+                Authorization: 'Bearer ' + params.token
             }
         })
     }
 
     createContact(data) {
         const dt = {
-            name:data.name,
-            phoneNumber:data.phoneNumber,
-            userId:data.userId,
-            bookId:data.bookId,
+            name: data.name,
+            phoneNumber: data.phoneNumber,
+            userId: data.userId,
+            bookId: data.bookId,
         }
 
         //console.log("check => ",api+"/contact/create", dt)
 
-        return axios.post(api+"/contact/create",dt,{
+        return axios.post(api + "/contact/create", dt, {
             'Content-Type': 'application/json',
-            Authorization : 'Bearer ' + data.token
+            Authorization: 'Bearer ' + data.token
         })
     }
 
